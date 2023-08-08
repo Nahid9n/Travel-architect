8f4aaca5ae76db916a8b8ab002e9e73989e79ba5
Nahid9n <91003844+Nahid9n@users.noreply.github.com>
2023-08-08 13:31:42 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,53 @@
+
+// Swiper js section Start here
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    loop:true,
+    autoplay:true,
+    grabCursor: true,
+    centeredSlides: true,
+    spaceBetween: 30,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 5,
+        stretch: 0,
+        depth: 150,
+        modifier: 1,
+        slideShadows: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+// Range Slider End here
+
+
+
+
+var slider2 = document.getElementById("myRange2");
+var output2 = document.getElementById("demo2");
+output2.innerHTML = slider2.value;
+
+slider2.oninput = function() {
+    output2.innerHTML = this.value;
+}
+
+// Range Slider 2 End here
+// Manipulate SwiperJs Start here
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+// Manipulate SwiperJs End here
+
